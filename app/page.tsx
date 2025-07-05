700e76a1327184a6d98b05d7ef0d697d286a2012
TechWithTwin <realwebtechguru@gmail.com>
2025-07-05 23:18:18 +0300

    adding two sets of animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,19 @@ import Card from "@/components/card";
 export default function Home() {
   return (
     <div className="mx-5 space-y-4 my-4">
-      <h1 className="text-3xl font-bold">Random Foxes</h1>
+      <h1 className="text-3xl font-bold text-center">
+        Random Foxes using Fade Up
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((item) => (
+        {Array.from({ length: 9 }, (_, i) => i + 1).map((item) => (
+          <Card key={item} item={item} />
+        ))}
+      </div>
+      <h1 className="text-3xl font-bold text-center">
+        Random Foxes using Zoom in
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {Array.from({ length: 9 }, (_, i) => i + 1).map((item) => (
           <Card key={item} item={item} />
         ))}
       </div>
