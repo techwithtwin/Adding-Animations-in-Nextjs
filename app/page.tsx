e8ce07b4bf10e8c4a82e19065eca71f9217c0f9c
TechWithTwin <realwebtechguru@gmail.com>
2025-07-05 23:15:43 +0300

    adding random foxes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Card from "@/components/card";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="mx-5 space-y-4 my-4">
+      <h1 className="text-3xl font-bold">Random Foxes</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((item) => (
+          <Card key={item} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+interface Props {
+  item: number;
+}
+
+const Card = ({ item }: Props) => {
+  return (
+    <div className="h-[30rem] overflow-hidden rounded-xl shadow">
+      <Image
+        className="w-full h-full object-cover"
+        src={`https://randomfox.ca/images/${item}.jpg`}
+        width={340}
+        height={340}
+        alt="random fox image"
+      />
+    </div>
+  );
 };
 
 export default Card;
